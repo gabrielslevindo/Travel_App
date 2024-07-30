97a33b6db123cbd175af70458064c452cbcbc869
Gabriel Levindo <69587938+gabrielslevindo@users.noreply.github.com>
2024-07-30 17:53:41 -0300

    [NavGraoh, #10, #4, #3, #2] -> create Fragment Login and add in navGraph

---
+++ app/src/main/java/com/example/travelapp/view/login/LoginFragment.kt
@@ -0,0 +1,37 @@
+package com.example.travelapp.view.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.travelapp.databinding.LoginScreenBinding
+
+class LoginFragment: Fragment() {
+    private var _binding: LoginScreenBinding? = null
+    private val binding: LoginScreenBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = LoginScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
